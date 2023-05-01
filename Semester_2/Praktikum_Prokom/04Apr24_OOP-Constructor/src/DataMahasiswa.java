1fd6454d7a97f25723b9fda232d5630099a83356
Bokibyte <triplecreeperx@gmail.com>
2023-05-01 19:25:14 +0700

    add proyek. yaallah lupa commit

---
+++ Semester_2/Praktikum_Prokom/04Apr24_OOP-Constructor/src/DataMahasiswa.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class DataMahasiswa {
+    protected ArrayList<String> NIM, nama, alamat, jurusan;
+    
+    public DataMahasiswa(){
+        NIM = new ArrayList<>();
+        nama =  new ArrayList<>();
+        alamat =  new ArrayList<>();
+        jurusan = new ArrayList<>();
+    }
+}
---
+++ Semester_2/Praktikum_Prokom/04Apr24_OOP-Constructor/src/Tools.java
@@ -0,0 +1,25 @@
+public class Tools extends DataMahasiswa{
+
+    // setter
+    public void addNIM(String input){
+        this.NIM.add(input);
+    }
+    public void addNama(String input){
+        this.nama.add(input);
+    }
+    public void addAlamat(String input){
+        this.alamat.add(input);
+    }
+    public void addJurusan(String input){
+        this.jurusan.add(input);
+    }
+
+    //getter
+    public void getNIM(int index){
+        this.NIM.get(index);
+    }
+    public void getNama(int index){
+        this.nama.get(index);
+    }
+    public
+}
