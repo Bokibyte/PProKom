724ca1299b194d8b5ccc12839a28b9cd888a93f6
Bokibyte <triplecreeperx@gmail.com>
2024-03-28 08:56:39 +0700

    deklarasi variabel awal

---
+++ Semester_2/Praktikum_Prokom/21.Mar.24_OOP/mainToko.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class mainToko {
+    public static void main(String[]args){
+        
+        ArrayList<String> itemsName = new ArrayList<>();
+        ArrayList<Integer> itemsStock = new ArrayList<>();
+        ArrayList<Integer> itemsPackedPrice = new ArrayList<>();
+        ArrayList<Integer> itemsPrcePerUnit = new ArrayList<>();
+
+        boolean showUtilityMenu = false;
+
+    }
+}
