2adad428ef9229eea01d454e1b20b9fc437b2d8c
Bokibyte <triplecreeperx@gmail.com>
2024-03-28 11:04:08 +0700

    selesai, tinggal tambah template untuk item awal

--- Semester_2/Praktikum_Prokom/21.Mar.24_OOP/mainToko.java
+++ Semester_2/Praktikum_Prokom/21.Mar.24_OOP/mainToko.java
@@ -1,39 +1,91 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class mainToko {
+
+    
+
     public static void setterMenu(){
         data_senin senin = new data_senin();
         data_selasa selasa = new data_selasa();
         data_rabu rabu = new data_rabu();
         Scanner sc = new Scanner(System.in);
         int setterInput = 0;
-        System.out.println("1. tambah item\n2. Total harga");
+        System.out.println("Tambah data pada hari:\n1. Senin\n2. Selasa\n3. Rabu");
         System.out.print("Masukan nomor menu: ");
         setterInput = sc.nextInt();
         if(setterInput==1){
-            setterInput = 0;
-            System.out.println("Hari:\n1. Senin\n2. Selasa\n3. Rabu");
-            System.out.print("Masukan nomor menu: ");
-            setterInput = sc.nextInt();
-            if(setterInput==1){
-                System.out.print("Masukan nama item: ");
-                senin.addItem(sc.next());
-                System.out.print("Masukan stok item: ");
-                senin.addStock(sc.nextInt());
-                System.out.print("Masukan harga item: ");
-                senin.addPrice(sc.nextInt());
-            }else if(setterInput==2){
-                
-            }
-        }
+            System.out.print("Masukan nama item: ");
+            senin.addItem(sc.next());
+            System.out.print("Masukan stok item: ");
+            senin.addStock(sc.nextInt());
+            System.out.print("Masukan harga item: ");
+            senin.addPrice(sc.nextInt());
+        }else if(setterInput==2){
+            System.out.print("Masukan nama item: ");
+            selasa.addItem(sc.next());
+            System.out.print("Masukan stok item: ");
+            selasa.addStock(sc.nextInt());
+            System.out.print("Masukan harga item: ");
+            selasa.addPrice(sc.nextInt());
+        }else if(setterInput==3){
+            System.out.print("Masukan nama item: ");
+            rabu.addItem(sc.next());
+            System.out.print("Masukan stok item: ");
+            rabu.addStock(sc.nextInt());
+            System.out.print("Masukan harga item: ");
+            rabu.addPrice(sc.nextInt());
+        }else{
+            System.out.println("input salah.");
+        }   
     }
+
     public static void main(String[]args){
         data_senin senin = new data_senin();
         data_selasa selasa = new data_selasa();
         data_rabu rabu = new data_rabu();
         Scanner sc = new Scanner(System.in);
+        int input = 0;
+        int totalPrice = 0;
+
         setterMenu();
         
+        System.out.println("Tampilkan toko pada hari?\n1. Senin\2. Selasa\3. Rabu");
+        System.out.print("Masukan input: ");
+        input = sc.nextInt();
+        if(input==1){
+            for(int i = 0; i < senin.itemAmount(); i++){
+                System.out.println();
+                System.out.println("Nama item = "+senin.getName(i));
+                System.out.println("Stok : "+senin.getStock(i));
+                System.out.println("Harga satuan : "+senin.getPrice(i));
+                System.out.println("Harga bolpoin : "+(senin.getStock(i)*senin.getPrice(i)));
+                totalPrice += (senin.getPrice(i)*senin.getStock(i));
+            }
+            System.out.println("Uang yang diterima jika semua item terjual: Rp"+totalPrice);
+
+        }else if(input==2){
+            for(int i = 0; i < selasa.itemAmount(); i++){
+                System.out.println();
+                System.out.println("Nama item = "+selasa.getName(i));
+                System.out.println("Stok : "+selasa.getStock(i));
+                System.out.println("Harga satuan : "+selasa.getPrice(i));
+                System.out.println("Harga bolpoin : "+(selasa.getStock(i)*selasa.getPrice(i)));
+                totalPrice += (selasa.getPrice(i)*selasa.getStock(i));
+            }
+            System.out.println("Uang yang diterima jika semua item terjual: Rp"+totalPrice);
+
+        }else if(input==3){
+            for(int i = 0; i < rabu.itemAmount(); i++){
+                System.out.println();
+                System.out.println("Nama item = "+rabu.getName(i));
+                System.out.println("Stok : "+rabu.getStock(i));
+                System.out.println("Harga satuan : "+rabu.getPrice(i));
+                System.out.println("Harga bolpoin : "+(rabu.getStock(i)*rabu.getPrice(i)));
+                totalPrice += (rabu.getPrice(i)*rabu.getStock(i));
+            }
+            System.out.println("Uang yang diterima jika semua item terjual: Rp"+totalPrice);
+        }else{
+            System.out.println("Input invalid.");
+        }
     }
 }
