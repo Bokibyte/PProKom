d24c51d1c47f3d6f2bfef8dab38aab079f4338d4
Bokibyte <triplecreeperx@gmail.com>
2024-03-28 10:18:57 +0700

    progress2

---
+++ Semester_2/Praktikum_Prokom/21.Mar.24_OOP/data_senin.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class data_senin {
+    private ArrayList<String> nama;
+    private ArrayList<Integer> stock, price;
+
+    public void addItem(String nama){
+        this.nama.add(nama);
+    }
+    public void addStock(int stock){
+        this.stock.add(stock);
+    }
+    public void addPrice(int price){
+        this.price.add(price);
+    }
+    public int itemAmount(){
+        return this.nama.size();
+    }
+    public String getName(int index){
+        return this.nama.get(index);
+    }
+    public int getStock(int index){
+        return this.stock.get(index);
+    }
+    public int getPrice(int index){
+        return this.price.get(index);
+    }
+}
--- Semester_2/Praktikum_Prokom/21.Mar.24_OOP/mainToko.java
+++ Semester_2/Praktikum_Prokom/21.Mar.24_OOP/mainToko.java
@@ -2,14 +2,38 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class mainToko {
+    public static void setterMenu(){
+        data_senin senin = new data_senin();
+        data_selasa selasa = new data_selasa();
+        data_rabu rabu = new data_rabu();
+        Scanner sc = new Scanner(System.in);
+        int setterInput = 0;
+        System.out.println("1. tambah item\n2. Total harga");
+        System.out.print("Masukan nomor menu: ");
+        setterInput = sc.nextInt();
+        if(setterInput==1){
+            setterInput = 0;
+            System.out.println("Hari:\n1. Senin\n2. Selasa\n3. Rabu");
+            System.out.print("Masukan nomor menu: ");
+            setterInput = sc.nextInt();
+            if(setterInput==1){
+                System.out.print("Masukan nama item: ");
+                senin.addItem(sc.next());
+                System.out.print("Masukan stok item: ");
+                senin.addStock(sc.nextInt());
+                System.out.print("Masukan harga item: ");
+                senin.addPrice(sc.nextInt());
+            }else if(setterInput==2){
+                
+            }
+        }
+    }
     public static void main(String[]args){
+        data_senin senin = new data_senin();
+        data_selasa selasa = new data_selasa();
+        data_rabu rabu = new data_rabu();
+        Scanner sc = new Scanner(System.in);
+        setterMenu();
         
-        ArrayList<String> itemsName = new ArrayList<>();
-        ArrayList<Integer> itemsStock = new ArrayList<>();
-        ArrayList<Integer> itemsPackedPrice = new ArrayList<>();
-        ArrayList<Integer> itemsPrcePerUnit = new ArrayList<>();
-
-        boolean showUtilityMenu = false;
-
     }
 }
