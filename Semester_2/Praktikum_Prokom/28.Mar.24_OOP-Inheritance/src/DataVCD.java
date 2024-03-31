6cfb8fd90557a333d424e44951e5e559b972ef74
Bokibyte <triplecreeperx@gmail.com>
2024-04-01 00:10:18 +0700

    inisialisasi & membuat konstruktor untuk var data

--- Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/DataVCD.java
+++ Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/DataVCD.java
@@ -5,5 +5,15 @@ public class DataVCD {
     private ArrayList<Integer> stok;
     private String[] kategori;
 
-    
+    public DataVCD(){
+        judul = new ArrayList<>();
+        aktor = new ArrayList<>();
+        sutradara = new ArrayList<>();
+        publisher = new ArrayList<>();
+        kategori = new String[3];
+        kategori[0] = "SU";
+        kategori[1] = "A";
+        kategori[2] = "R";
+        kategori[3] = "D";
+    }
 }
