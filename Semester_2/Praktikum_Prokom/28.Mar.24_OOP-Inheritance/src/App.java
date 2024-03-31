2e8e8b94ce8ae888619363445b5f0d55adfe0bcb
Bokibyte <triplecreeperx@gmail.com>
2024-04-01 03:10:36 +0700

    oke sudah lanjut ngerangkai

--- Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/App.java
+++ Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/App.java
@@ -1,44 +1,42 @@
 import javax.swing.JOptionPane;
 
 public class App {
+    static AppMethods methods = new AppMethods();
+    static String input;
 
-    public static void addMenu(){
-        AppMethods methods = new AppMethods();
-        String input = "";
-        boolean passCheck = false;
-
-        input = JOptionPane.showInputDialog(null, "Masukan VCD anda: ", "Menu Judul", JOptionPane.QUESTION_MESSAGE);
+    public static void judulMenu(){
+        input = JOptionPane.showInputDialog(null, "Masukan VCD anda: ", "Menu Judul", JOptionPane.PLAIN_MESSAGE);
         methods.addJudul(input);
+    }
+    public static void aktorMenu(){
         input = JOptionPane.showInputDialog(null, "Masukan aktor-aktornya: ", "Menu aktor", JOptionPane.QUESTION_MESSAGE);
         methods.addAktor(input);
+    }
+    public static void sutradaraMenu(){
         input = JOptionPane.showInputDialog(null, "Masukan Sutradaranya: ", "Menu Sutradara", JOptionPane.QUESTION_MESSAGE);
         methods.addSutradara(input);
+    }
+    public static void publisherMenu(){
         input = JOptionPane.showInputDialog(null, "Masukan Publishernya: ", "Menu Publisher", JOptionPane.QUESTION_MESSAGE);
         methods.addPublisher(input);
+    }
+    public static void ratingMenu(){
         input = String.valueOf(JOptionPane.showOptionDialog(null, "Pilih rating VCD: ", "Rating Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, methods.kategoriList, methods.kategoriList[0]));
         methods.addRating(Integer.parseInt(input));
-
-        while(!passCheck){
+    }
+    public static void stokMenu(){
+        while(true){
+            input = JOptionPane.showInputDialog(null, "Masukan stok VCD: ", "Menu Stok", JOptionPane.QUESTION_MESSAGE);
             try{
-                input = JOptionPane.showInputDialog(null, "Masukan stok VCD anda: ", "Menu Stok", JOptionPane.QUESTION_MESSAGE);
                 methods.addStok(Integer.parseInt(input));
-                if(Integer.parseInt(input) != JOptionPane.CLOSED_OPTION){
-                    methods.addStok(0);
-                }
+                break;
             }catch(NumberFormatException e){
-                JOptionPane.showMessageDialog(null, "Input bukan integer!", "Checking Menu", JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Input invalid!", "Check Menu", JOptionPane.ERROR_MESSAGE);
             }
         }
-        
-        
     }
 
     public static void main(String[] args) throws Exception {
 
-        for(int i = 0; i < 5; i++){
-            addMenu();
-        }
-
-        
     }
 }
