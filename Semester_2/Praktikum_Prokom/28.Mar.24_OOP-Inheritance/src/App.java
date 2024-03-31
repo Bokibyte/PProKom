9fa8e4145f96d5c76ad41fca654e0780ceda0f33
Bokibyte <triplecreeperx@gmail.com>
2024-04-01 05:10:23 +0700

    debugging time

--- Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/App.java
+++ Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/App.java
@@ -20,69 +20,89 @@ public class App {
             methods.addJudul(input);
             isPass = true;
         }else{
-            methods.addJudul(input);
+            methods.setJudul(editIndex, input);
             isPass = true;
         }
         return isPass;
     }
 
-    public static boolean aktorMenu(){
+    public static boolean aktorMenu(int editIndex){
         boolean isPass = false;
+        boolean isCancel = false;
         input = JOptionPane.showInputDialog(null, "Masukan aktor-aktornya: ", "Menu aktor", JOptionPane.QUESTION_MESSAGE);
         if(input == null){
             isPass = false;
-        }else{
+        }else if(!isCancel){
             methods.addAktor(input);
+            isPass = true;
+        }else{
+            methods.setAktor(editIndex, input);
             isPass = true;
         }
         return isPass;
     }
 
-    public static boolean sutradaraMenu(){
+    public static boolean sutradaraMenu(int editIndex){
         boolean isPass = false;
+        boolean isCancel = false;
         input = JOptionPane.showInputDialog(null, "Masukan Sutradaranya: ", "Menu Sutradara", JOptionPane.QUESTION_MESSAGE);
         if(input == null){
             isPass = false;
-        }else{
+        }else if(!isCancel){
             methods.addSutradara(input);
+            isPass = true;
+        }else{
+            methods.setSutradara(editIndex, input);
             isPass = true;
         }
         return isPass;
     }
 
-    public static boolean publisherMenu(){
+    public static boolean publisherMenu(int editIndex){
         boolean isPass = false;
+        boolean isCancel = false;
         input = JOptionPane.showInputDialog(null, "Masukan Publishernya: ", "Menu Publisher", JOptionPane.QUESTION_MESSAGE);
         if(input == null){
             isPass = false;
-        }else{
+        }else if(!isCancel){
             methods.addPublisher(input);
+            isPass = true;
+        }else{
+            methods.setPublisher(editIndex, input);
             isPass = true;
         }
         return isPass;
     }
     
-    public static boolean ratingMenu(){
+    public static boolean ratingMenu(int editIndex){
         boolean isPass = false;
+        boolean isCancel = false;
         input = String.valueOf(JOptionPane.showOptionDialog(null, "Pilih rating VCD: ", "Rating Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, methods.kategoriList, methods.kategoriList[0]));
         if(input == null){
             isPass = false;
-        }else{
+        }else if(!isCancel){
             methods.addRating(Integer.parseInt(input));
+            isPass = true;
+        }else{
+            methods.setRating(editIndex, editIndex);
             isPass = true;
         }
         return isPass;
     }
 
-    public static boolean stokMenu(){
+    public static boolean stokMenu(int editIndex){
         boolean isPass = false;
+        boolean isCancel = false;
         while(true){
             input = JOptionPane.showInputDialog(null, "Masukan stok VCD: ", "Menu Stok", JOptionPane.QUESTION_MESSAGE);
             try{
                 if(input == null){
                     isPass = false;
-                }else{
+                }else if(!isCancel){
                     methods.addStok(Integer.parseInt(input));
+                    isPass = true;
+                }else{
+                    methods.setStok(editIndex, Integer.parseInt(input));
                     isPass = true;
                 }
                 return isPass;
@@ -92,31 +112,36 @@ public class App {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void runMenu(){
         int indexEdit = 0;
         boolean menuPass = false;
-
         while(true){
             indexEdit = methods.getSize();
             while(true){
-                menuPass = judulMenu();
+                menuPass = judulMenu(indexEdit);System.out.println(methods.judulList);
                 if(menuPass){
-                    menuPass = aktorMenu();
+                    menuPass = aktorMenu(indexEdit);System.out.println(methods.aktorList);
                 }else{
                     break;
                 }if (menuPass){
-                    menuPass = sutradaraMenu();
+                    menuPass = sutradaraMenu(indexEdit);System.out.println(methods.sutradaraList);
                 }if (menuPass){
-                    menuPass = publisherMenu();
+                    menuPass = publisherMenu(indexEdit);System.out.println(methods.publisherList);
                 }if (menuPass){
-                    menuPass = ratingMenu();
+                    menuPass = ratingMenu(indexEdit);System.out.println(methods.ratingList);
                 }if (menuPass){
-                    menuPass = stokMenu();
+                    menuPass = stokMenu(indexEdit);System.out.println(methods.ratingList);
                 }if (menuPass){
                     break;
+                    
                 }
             }
             break;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        runMenu();
+        
     }
 }
