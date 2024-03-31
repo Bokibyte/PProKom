a73df8b1987bd7c27ebc407f54d0359173f97db6
Bokibyte <triplecreeperx@gmail.com>
2024-04-01 00:00:45 +0700

    membuat variabel data

--- Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/DataVCD.java
+++ Semester_2/Praktikum_Prokom/28.Mar.24_OOP-Inheritance/src/DataVCD.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
 public class DataVCD {
+    private ArrayList<String> judul, aktor, sutradara, publisher;
+    private ArrayList<Integer> stok;
+    private String[] kategori;
+
     
 }
