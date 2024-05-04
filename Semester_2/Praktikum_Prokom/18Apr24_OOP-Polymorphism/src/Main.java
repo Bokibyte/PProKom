ebc478a202d27b40c4239e9481f0825a7c3b417e
Bokibyte <triplecreeperx@gmail.com>
2024-05-04 17:23:51 +0700

    done!

--- Semester_2/Praktikum_Prokom/18Apr24_OOP-Polymorphism/src/Main.java
+++ Semester_2/Praktikum_Prokom/18Apr24_OOP-Polymorphism/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void OperasiBilanganCetak(OperasiBilangan[] oB){
         
+        for(int i = 0; i < oB.length; i++){
+            oB[i].tampil();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        OperasiBilangan[] oB = {new OperasiPenjumlahan(),
+                                new OperasiPengurangan(),
+                                new OperasiPerkalian(),
+                                new OperasiPembagian()};
+        OperasiBilanganCetak(oB);
     }
 }
